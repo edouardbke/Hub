bd4a50dbe5edcf3dae7762a3d559f9a33a111c2b
edouardBke <edouardmakenga0@gmail.com>
2022-06-03 18:40:04 +0200

    second change v1.11

--- System/Hub.py
+++ System/Hub.py
@@ -13,7 +13,7 @@ class Hub :
 
 
                 for device in devices:
-                    if device[1] not in os.system('hcitool -con'):
+                    if device.split()[1] not in os.system('hcitool con'):
                         print(devices)
                         print(device)
                         self.devicelist = devices
